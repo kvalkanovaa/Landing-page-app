39d2125f8ff908c9dccfea4deefd33bc10c036a0
kalina09 <k.valkanova@softavail.com>
2024-10-21 18:31:46 +0300

    Add Bootstrap and Sass setup for styling

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      additionalData: `
+      @import "src/sass/variables";
+      @import "node_modules/bootstrap/scss/bootstrap";
+    `,
+    },
+  },
 });
