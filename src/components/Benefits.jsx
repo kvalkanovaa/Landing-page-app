1a90b7c21c81bee8cbe27100a59080315c954efc
kalina09 <k.valkanova@softavail.com>
2024-10-25 05:34:03 +0300

    Update benefits component and its styling

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -24,25 +24,51 @@ const Benefits = () => {
   ];
 
   return (
-    <section className="container mt-5">
-      <div className="row">
-        <h2 className="text-center mb-5 p-5">With every myPOS payment terminal you get:</h2>
+    <section className="container pb-md-5">
+      <div className="row g-5">
+        <h2 className="text-center text-muted mb-0 mb-lg-5">
+          With every myPOS payment terminal you get:
+        </h2>
         {benefits.map((benefit, index) => (
           <div
             key={index}
-            className="col-12 col-md-6 mb-4 px-5 d-flex justify-content-start"
+            className="col-12 col-md-6 mb-4 d-flex justify-content-start"
           >
-            <div className="d-flex gap-3 justify-content-start align-items-center">
-              <img
-                src={benefit.img}
-                alt={benefit.text}
-                className=""
-                style={{ height: '50px' }}
-              />
-              <div className="d-flex flex-column justify-content-center align-items-start">
-                <p className="m-0">{benefit.text}</p>
+            <div className="row d-flex justify-content-center align-items-center g-2">
+              <div className="d-flex gap-3 justify-content-start align-items-center">
+                <img
+                  src={benefit.img}
+                  alt={benefit.text}
+                  className="benefit-img" // I would have used img-fluid class if the images were the same size
+                />
+                <div className="d-flex flex-column justify-content-center align-items-start">
+                  <h4 className="m-0 text-muted">{benefit.text}</h4>
+                  <b>{benefit.boldText}</b>
+                </div>
+              </div>
+              {/* If the imgs were the same size I would have used this structure to avoid fixed width and height */}
+              {/* <div className="col-2 col-lg-4">
+                <img
+                  src={benefit.img}
+                  alt={benefit.text}
+                  className="img-fluid" !!!!!
+                  // Since the icons are different sizes, when we use img-fluid class
+                  // it is not working properly - some icons are bigger than others
+                  // It is always better the icons to be the same size to avoid this issue.
+                  // But since the icons are different sizes, we will use a fixed width for all.
+                  // Also we have to use media queries to make them smaller on smaller screens
+                  // Generally, if the images are the same size in the design,
+                  // we could use the img-fluid class to make them responsive.
+                  // Also I would have used svgs instead of pngs for better quality and scalability
+                  // but I couldn't make it work properly with the psd file - the svgs were containing
+                  // the whole design and that made them very large in size and not suitable for this purpose.
+                />
+              </div>
+              <div className="col-10 col-lg-8 d-flex flex-column justify-content-center align-items-start ps-4">
+                <h4 className="m-0 text-muted">{benefit.text}</h4>
                 <b>{benefit.boldText}</b>
               </div>
+              */}
             </div>
           </div>
         ))}
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.js';
 import './sass/custom.scss';
 import App from './App.jsx';
 
